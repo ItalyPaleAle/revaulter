1063cada0184d6a7d1d18837ecb2db3f7f6d7315
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2026-01-10 14:06:40 -0800

    No icons in manifest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,18 +21,7 @@ export default defineConfig(({ mode }) => {
                     theme_color: '#1e293b',
                     background_color: '#f8fafc',
                     display: 'standalone',
-                    icons: [
-                        {
-                            src: '/icon-192.png',
-                            sizes: '192x192',
-                            type: 'image/png',
-                        },
-                        {
-                            src: '/icon-512.png',
-                            sizes: '512x512',
-                            type: 'image/png',
-                        },
-                    ],
+                    icons: [],
                 },
                 devOptions: {
                     enabled: false,
