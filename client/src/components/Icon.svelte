86f0032f7bd60c9bc908044238ed98c7352ebbd1
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2026-04-14 21:39:00 -0700

    Replaced all icons with Lucide icons

--- client/src/components/Icon.svelte
+++ client/src/components/Icon.svelte
@@ -10,103 +10,68 @@ interface Props {
 const { icon, title, size = '12' }: Props = $props()
 </script>
 
+<!--
+    All icons from lucide.dev
+    Copyright (c) Lucide Contributors
+    Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
+-->
 <i class="w-{size}">
     {#if icon == 'lock-open'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
+            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+            <path d="M7 11V7a5 5 0 0 1 9.9-1" />
         </IconContainer>
     {:else if icon == 'lock-closed'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+            <path d="M7 11V7a5 5 0 0 1 10 0v4" />
         </IconContainer>
     {:else if icon == 'key'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+            <path d="m15.5 7.5 2.3 2.3a1 1 0 0 0 1.4 0l2.1-2.1a1 1 0 0 0 0-1.4L19 4" />
+            <path d="m21 2-9.6 9.6" />
+            <circle cx="7.5" cy="15.5" r="5.5" />
         </IconContainer>
     {:else if icon == 'clock'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <circle cx="12" cy="12" r="10" />
+            <path d="M12 6v6l4 2" />
         </IconContainer>
     {:else if icon == 'note'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+            <path d="M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z" />
+            <path d="M7 11h10" />
+            <path d="M7 15h6" />
+            <path d="M7 7h8" />
         </IconContainer>
     {:else if icon == 'check-circle'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <circle cx="12" cy="12" r="10" />
+            <path d="m9 12 2 2 4-4" />
         </IconContainer>
     {:else if icon == 'pencil'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+            <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+            <path d="m15 5 4 4" />
         </IconContainer>
     {:else if icon == 'check-badge'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+            <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+            <path d="m9 12 2 2 4-4" />
         </IconContainer>
     {:else if icon == 'x-circle'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <circle cx="12" cy="12" r="10" />
+            <path d="m15 9-6 6" />
+            <path d="m9 9 6 6" />
         </IconContainer>
     {:else if icon == 'data'}
-        <!--
-            heroicons.com
-            Copyright (c) 2020 Refactoring UI Inc
-            Licensed under MIT license: https://github.com/tailwindlabs/heroicons/blob/master/LICENSE
-        -->
         <IconContainer {title} {size}>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+            <ellipse cx="12" cy="5" rx="9" ry="3" />
+            <path d="M3 5V19A9 3 0 0 0 21 19V5" />
+            <path d="M3 12A9 3 0 0 0 21 12" />
         </IconContainer>
     {:else if icon == 'settings'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M14 17H5" />
             <path d="M19 7h-9" />
@@ -114,21 +79,11 @@ const { icon, title, size = '12' }: Props = $props()
             <circle cx="7" cy="7" r="3" />
         </IconContainer>
     {:else if icon == 'key-round'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M15.5 14.5 18 17l4-4" />
             <circle cx="7.5" cy="15.5" r="5.5" />
         </IconContainer>
     {:else if icon == 'refresh-cw'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M21 12a9 9 0 0 0-15.5-6.36L3 8" />
             <path d="M3 3v5h5" />
@@ -136,70 +91,35 @@ const { icon, title, size = '12' }: Props = $props()
             <path d="M16 16h5v5" />
         </IconContainer>
     {:else if icon == 'shield'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10" />
         </IconContainer>
     {:else if icon == 'log-out'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
             <path d="M16 17l5-5-5-5" />
             <path d="M21 12H9" />
         </IconContainer>
     {:else if icon == 'clipboard-copy'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
             <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
         </IconContainer>
     {:else if icon == 'check'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M20 6 9 17l-5-5" />
         </IconContainer>
     {:else if icon == 'x'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M18 6 6 18" />
             <path d="m6 6 12 12" />
         </IconContainer>
     {:else if icon == 'user'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
             <circle cx="12" cy="7" r="4" />
         </IconContainer>
     {:else if icon == 'fingerprint'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M12 10a2 2 0 0 0-2 2c0 1.02-.1 2.51-.26 4" />
             <path d="M14 13.12c0 2.38 0 6.38-1 8.88" />
@@ -212,22 +132,12 @@ const { icon, title, size = '12' }: Props = $props()
             <path d="M9 6.8a6 6 0 0 1 9 5.2v2" />
         </IconContainer>
     {:else if icon == 'trash'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M3 6h18" />
             <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
             <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
         </IconContainer>
     {:else if icon == 'plus'}
-        <!--
-            lucide.dev
-            Copyright (c) Lucide Contributors
-            Licensed under ISC license: https://github.com/lucide-icons/lucide/blob/main/LICENSE
-        -->
         <IconContainer {title} {size}>
             <path d="M5 12h14" />
             <path d="M12 5v14" />
